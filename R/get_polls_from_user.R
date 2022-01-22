60dc55d312014b4b1cbcbff03b437990d3bd15ec
gn385x <giang.nglinh@gmail.com>
2022-01-21 18:41:43 -0800

    Change example

--- R/get_polls_from_user.R
+++ R/get_polls_from_user.R
@@ -1,17 +1,17 @@
-#' #' Get Polls from User
-#' #'
-#' #' Return a data.frame of poll ids for a given Twitter user.
-#' #' REQUIRES: Credentials for the Twitter API need to be stored as environment 
-#' #' variables since this function calls directly to the bash_profile.
-#' #'
-#' #' @param username character, the username of the twitter user to query
-#' #'
-#' #' @return data.frame
-#' #' @export
-#' #'
-#' #' @importFrom magrittr %>%
-#' #' @import twitteR tidyverse
-#' #' @examples
-#' #' \dontrun{
-#' #'   get_poll_from_user('@ChipotleTweets')
-#' #' }
+#' Get Polls from User
+#'
+#' Return a data.frame of poll ids for a given Twitter user.
+#' REQUIRES: Credentials for the Twitter API need to be stored as environment 
+#' variables since this function calls directly to the bash_profile.
+#'
+#' @param username character, the username of the twitter user to query
+#'
+#' @return data.frame
+#' @export
+#'
+#' @importFrom magrittr %>%
+#' @import twitteR tidyverse
+#' @examples
+#' \dontrun{
+#'   get_poll_from_user('@ChipotleTweets')
+#' }
