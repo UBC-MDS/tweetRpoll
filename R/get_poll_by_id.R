ccaf63af6b0997f20f2bf201011c5e3eaf244e7b
gn385x <giang.nglinh@gmail.com>
2022-01-21 18:44:22 -0800

    Add docstring for get_poll_by_id

---
+++ R/get_poll_by_id.R
@@ -0,0 +1,18 @@
+# +
+#' Get Poll Data from Poll ID
+#'
+#' Return a data.frame of poll data from Twitter given a poll ID. 
+#' The output data.frame will contain five columns: (1) poll_question,
+#' (2) poll_options, (3) total_responses, (4) duration, (5) date.
+#'
+#' @param poll_id character, the id of the twitter poll to query
+#'
+#' @return data.frame
+#' @export
+#'
+#' @importFrom magrittr %>%
+#' @import twitteR tidyverse
+#' @examples
+#' \dontrun{
+#'   get_poll_by_id('4235234')
+#' }
