925bf3975769d306889b1e87103903cc9a4f4390
Rada Rudyak <radawilinofsky@gmail.com>
2022-01-28 04:12:41 -0800

    made it a function

--- R/get_poll_by_id.R
+++ R/get_poll_by_id.R
@@ -4,7 +4,7 @@
 #' The output data.frame will contain five columns: (1) poll_question,
 #' (2) poll_options, (3) total_responses, (4) duration, (5) date.
 #'
-#' @param poll_id character, the id of the twitter poll to query
+#' @param tweet_id character, the id of the tweet with poll to query
 #'
 #' @return data.frame
 #' @export
@@ -17,49 +17,58 @@
 #' }
 
 require(httr)
+require(jsonlite)
+require(dplyr)
+require(tidyr)
 
-bearer_token <- "AAAAAAAAAAAAAAAAAAAAAAyIYQEAAAAAjvBdCMMh1dT8clkpXhHxzld7Dhs%3DLPl5zMXXOZqznZGe9JP7zHj3Wzx0N4unogLcWl8wfIkwikjQKm"
+get_poll_by_id <- function(tweet_id) {
 
-headers <- c(`Authorization` = sprintf('Bearer %s', bearer_token))
-url_string <- 'https://api.twitter.com/2/tweets/1485680107515564032'
+  numbers <- "1234567890"
+  numbers_only <- function(x) !grepl("\\D", x)
 
-params = list(
-  `expansions` = 'attachments.poll_ids'
-)
-
-response <- httr::GET(url = url_string, httr::add_headers(.headers=headers), query = params)
-print(response)
-
-obj <- httr::content(res, as = "text")
-print(obj)
-
-  if (!is.character(username)) {
-    stop("Invalid argument type: input username must be a string.")
+  if (!numbers_only(tweet_id)) {
+    stop('Please enter a valid tweet id')
   }
-  if (!is.numeric(tweet_num) | tweet_num%%1 != 0 | tweet_num < 5 | tweet_num > 100) {
-    stop('Invalid argument: input tweet_num must be an interger >= 5 and <= 100.')
-  }
-  
-  bearer_token <- "AAAAAAAAAAAAAAAAAAAAAOcOYQEAAAAArkXS3vK8LeepFyFpQEnoXoFheTg%3Dx86kO3cGNAt0jnap6njmh2cMJoBTjUXMlkrfIUDuW6YlvXWaf8"
+
+  bearer_token <- "AAAAAAAAAAAAAAAAAAAAAAyIYQEAAAAAjvBdCMMh1dT8clkpXhHxzld7Dhs%3DLPl5zMXXOZqznZGe9JP7zHj3Wzx0N4unogLcWl8wfIkwikjQKm"
+
   headers <- c(`Authorization` = sprintf('Bearer %s', bearer_token))
-  
-  user_params = list()
-  user_url <- sprintf('https://api.twitter.com/2/users/by?usernames=%s', username)
-  user_response <- httr::GET(url = user_url, httr::add_headers(.headers = headers), query = user_params)
-  user_object <- httr::content(user_response, as = "text")
-  user_json_data <- as.data.frame(fromJSON(user_object))
-  user_id <-sprintf(user_json_data$data.id)
+  url_string <- sprintf('https://api.twitter.com/2/tweets/%s', tweet_id)
+  expansions = 'attachments.poll_ids,author_id'
 
-  tweet_params = list(`expansions` = 'attachments.poll_ids', `max_results` = tweet_num)
-  tweet_url <- sprintf('https://api.twitter.com/2/users/%s/tweets', user_id)
-  tweet_response <- httr::GET(url = tweet_url, httr::add_headers(.headers = headers), query = tweet_params)
-  
+  params = list(
+    'expansions' = 'attachments.poll_ids,author_id',
+    'poll.fields' = 'duration_minutes,end_datetime'
+  )
+
+  response <- httr::GET(url = url_string, httr::add_headers(.headers=headers), query = params)
+
   tweet_object <- content(
-    tweet_response,
+    response,
     as = 'parsed',
     type = 'application/json',
     simplifyDataFrame = TRUE
   )
-  
-  poll_id <- tweet_object$includes$polls[1]
-  poll_id
+
+#  if (tweet_object$errors$title == "Not Found Error") {
+#    stop('Not a valid tweet ID')
+#  }
+
+#  if (tweet_object$error$title == "No Poll") {
+#    stop('Tweet does not contain a poll')
+#  }
+
+
+  res <- list()
+  res['text'] <- tweet_object$data$text
+  res['duration'] <- tweet_object$includes$polls$duration_minutes
+  res['date'] <- tweet_object$includes$polls$end_datetime
+  res['poll options'] <- tweet_object$includes$polls$options
+  res['user'] <- tweet_object$includes$users$username
+  res['total'] <- sum(tweet_object$includes$polls$options[[1]]$votes)
+
+  return (res)
+}
+
+#poll_obj <- get_poll_by_id('1481040318325739523')
+
