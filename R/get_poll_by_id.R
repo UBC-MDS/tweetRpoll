040993f86a5f26b6d58da4422d8a82937b6c39ce
Rada Rudyak <radawilinofsky@gmail.com>
2022-01-28 03:36:48 -0800

    got response working

--- R/get_poll_by_id.R
+++ R/get_poll_by_id.R
@@ -33,3 +33,33 @@ print(response)
 obj <- httr::content(res, as = "text")
 print(obj)
 
+  if (!is.character(username)) {
+    stop("Invalid argument type: input username must be a string.")
+  }
+  if (!is.numeric(tweet_num) | tweet_num%%1 != 0 | tweet_num < 5 | tweet_num > 100) {
+    stop('Invalid argument: input tweet_num must be an interger >= 5 and <= 100.')
+  }
+  
+  bearer_token <- "AAAAAAAAAAAAAAAAAAAAAOcOYQEAAAAArkXS3vK8LeepFyFpQEnoXoFheTg%3Dx86kO3cGNAt0jnap6njmh2cMJoBTjUXMlkrfIUDuW6YlvXWaf8"
+  headers <- c(`Authorization` = sprintf('Bearer %s', bearer_token))
+  
+  user_params = list()
+  user_url <- sprintf('https://api.twitter.com/2/users/by?usernames=%s', username)
+  user_response <- httr::GET(url = user_url, httr::add_headers(.headers = headers), query = user_params)
+  user_object <- httr::content(user_response, as = "text")
+  user_json_data <- as.data.frame(fromJSON(user_object))
+  user_id <-sprintf(user_json_data$data.id)
+
+  tweet_params = list(`expansions` = 'attachments.poll_ids', `max_results` = tweet_num)
+  tweet_url <- sprintf('https://api.twitter.com/2/users/%s/tweets', user_id)
+  tweet_response <- httr::GET(url = tweet_url, httr::add_headers(.headers = headers), query = tweet_params)
+  
+  tweet_object <- content(
+    tweet_response,
+    as = 'parsed',
+    type = 'application/json',
+    simplifyDataFrame = TRUE
+  )
+  
+  poll_id <- tweet_object$includes$polls[1]
+  poll_id
